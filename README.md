8bd77d59f6c17601fab4d2096286a07dbfc92914
XBlayz <stefano.scarcelli15@gmail.com>
2023-04-02 20:38:06 +0200

    Solution v1.0

--- README.md
+++ README.md
@@ -1,21 +1,33 @@
 # HashingPuzzle
 A little *puzzle* on using **multithreading** to force a **hash function**.
 
-# The following problem is defined as a hashingpuzzle:
-* Let S be an input string, Dun an integer and H a hashing algorithm.
-* The goal is to identify a key K such that H(S+K) = T, where S+K is the concatenation between If Ke and T must start with Dzeri.
+## The following problem is defined as a hashing puzzle
+* Let **S** be an input *string*, **D** un an *integer* and **H** a *hashing algorithm*.
+* The goal is to identify a key **K** such that **H**(**S**+**K**) = **T**, where **S**+**K** is the *concatenation* between the two strings and **T** must start with **D** zeros.
 
-# Write a multithreaded application to solve the following puzzle:
-* S= SisOp-Course A-Hashing-Puzzle-
-* D= 7H
-* The hashing algorithm to be used is SHA3- 256.
-* The key K to be identified is a string of length between 1 and 6, which can contain only the characters of the following alphabet:
-: ; < = > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [\]
+## Write a multithreaded application to solve the following puzzle
+* S = `SisOp-Course A-Hashing-Puzzle-`
+* D = `7`
+* The *hashing algorithm* to be used is `SHA3- 256`.
+* The key **K** to be identified is a *string* of length between *1* and *6*, which can contain only the characters of the following alphabet:
 
-# Hint:
+`: ; < = > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ]`
+
+## Hint
 * The characters of the given alphabet are all consecutive...
 * A char can be mapped to an integer...
 
-# How to build
-* The Java implementation of SHA3-256 to be used is the one provided by apache.commons.codec (a Maven project is required).
-* To use the library, insert the following dependencies in the pom.xml file:
+### Example
+* **S** = `Esempio-Hashing-Puzzle-`
+* **D** = *4*
+* **K** = `=MZ`
+
+**T** = **H**("Esempio-Hashing-Puzzle-*=MZ*") = ***0000***b5114f4f2da3eb004bf117eac13a1f02ccf459635de08d35457a40b3d515
+
+---
+
+#### Puzzle credit:
+- *Prof.* **F. Marozzo**
+- *Prof.* **R. Cantini**
+
+***UNICAL University***, Calabria, Italy.
---
+++ hashing-puzzle/src/main/java/xblayz/hashapp/HashFinder.java
@@ -0,0 +1,87 @@
+package xblayz.hashapp;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.codec.digest.MessageDigestAlgorithms;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class HashFinder implements Runnable {
+    private final int n;        // Number of characters
+    private final byte cMin;    // Byte representation of the first character
+    private final int d;        // Number of 0 that the string must have
+    private final String zeros;
+    private final byte[] s;     // Byte array of the S string
+
+    private final long end;     // Ending index of the search space
+
+    private long index;         // Index of the last tested K string
+    private int lastKLen = 0;   // Length of the last K string
+    private long strOffset;     // Number of string with length less then the length of the current K string
+
+    public HashFinder(int n, byte cMin, int d, String s, long start, long end) {
+        this.n = n;
+        this.cMin = cMin;
+        this.d = d;
+        this.zeros = new String(new char[this.d]).replace('\0', '0');
+        this.s = s.getBytes();
+
+        this.end = end;
+        this.index = start;
+    }
+
+    @Override
+    public void run() {
+        while (this.index <= end) {
+            byte[] str = ArrayUtils.addAll(this.s, this.getNextK());    // Concatenation of the S and K string
+
+            String t = new DigestUtils(MessageDigestAlgorithms.SHA3_256).digestAsHex(str);  // Hashing of S+K
+
+            if(t.substring(0, this.d).equals(this.zeros)){   // Check of the problem condition
+                System.out.println(t);
+                break;
+            }
+        }
+    }
+
+    private byte[] getNextK() {
+        int len = this.kLen();          // Calcolate the length of the index-th string K
+        byte[] b = new byte[len];       // Initializing byte array
+
+        if(this.lastKLen != len){                               // Check if the new string to generate is longer of the previous
+            this.strOffset = geometricSeries(this.n, len-1);    // Set the offset to the number of string with length from 1 to len-1
+            this.lastKLen = len;                                // Set the new previous string length
+        }
+
+        long a = index - 1 - this.strOffset;    // Number of the string with length len to calculate
+        for(int i=0; i<len; i++){
+            if(a <= 0)
+                b[i] = this.cMin;                       // Fill the empty space with the cMin character
+
+            // Calculate the character of the position i using the formula for changing base to a number
+            b[i] = (byte)((a % this.n) + this.cMin);
+            a /= this.n;
+        }
+        this.index++;   // Increment the index
+
+        return b;
+    }
+
+    private int kLen() {
+        int p = 1;
+        long minIndex = 1;                              // Index of the first string of length 1
+        long maxIndex = geometricSeries(this.n, p);     // Index of the last string of length 1
+
+        while (true) {
+            if(this.index >= minIndex && this.index <= maxIndex) break;     // Check if index is of the string of length p 
+
+            p++;                                    // Try next p
+            // Set new bound
+            minIndex = maxIndex;
+            maxIndex = geometricSeries(this.n, p);
+        }
+        return p;
+    }
+
+    private static long geometricSeries(int n, int p) {
+        return (long)((n - Math.pow(n, p+1)) / (1-n));  // Geometric series formula
+    }
+}
